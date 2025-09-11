7ab18a65a77f40b783bdb117668a1f5618d00814
Dahyeon Jin <wlsekgus12@gmail.com>
2025-09-11 19:05:58 +0900

    design: Navar 반응형 추가

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -19,8 +19,11 @@ const Navbar = (): JSX.Element => {
   };
 
   return (
-    <header className="sticky top-0 z-10 mx-auto px-60 py-3 h-14 flex items-center justify-between bg-white">
-      <div className="flex items-center space-x-14">
+    <header
+      className="sticky top-0 z-10 mx-auto
+      px-5 sm:px-8 md:px-16 lg:px-32 xl:px-60 py-3 h-14
+      flex items-center justify-between bg-white">
+      <div className="flex items-center md:space-x-12 lg:space-x-14">
         <Image
           src="/design-k-korea-logo.png"
           alt="design-k-korea logo"
@@ -28,22 +31,28 @@ const Navbar = (): JSX.Element => {
           height={36}
           priority
         />
-        {navigationPages.map(navPage => (
-          <Link
-            key={navPage.id}
-            href={navPage.href}
-            className="text-[#465A6E] font-bold hover:text-gray-800 transition-colors flex items-center">
-            {navPage.text}
-            {navPage.hasDropdown && (
-              <ChevronDown className="w-4 h-4 ml-1" onClick={handleDropdown} />
-            )}
-          </Link>
-        ))}
+        <div className="hidden md:flex items-center space-x-8 lg:space-x-14">
+          {navigationPages.map(navPage => (
+            <Link
+              key={navPage.id}
+              href={navPage.href}
+              className="text-[#465A6E] font-bold hover:text-gray-800 transition-colors flex items-center text-sm lg:text-base">
+              {navPage.text}
+              {navPage.hasDropdown && (
+                <ChevronDown
+                  className="w-4 h-4 ml-1"
+                  onClick={handleDropdown}
+                />
+              )}
+            </Link>
+          ))}
+        </div>
       </div>
+
       <nav className="flex items-center space-x-6">
         <Link
           href="/request"
-          className="text-[#4E5968] font-bold bg-[#F2F4F6] px-4 py-2 rounded-md hover:text-gray-800 hover:bg-[#E5E7EB] transition-colors">
+          className="text-[#4E5968] text-sm lg:text-base font-bold bg-[#F2F4F6] px-3 md:px-4 py-2 rounded-md hover:text-gray-800 hover:bg-[#E5E7EB] transition-colors">
           의뢰하기
         </Link>
       </nav>
